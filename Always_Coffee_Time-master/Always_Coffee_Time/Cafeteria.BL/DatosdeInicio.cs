81884da8b9b931ab1141abed0bdef576130b6b18
Karolgonzalez <gisellcastro83@gmail.com>
2022-04-28 22:09:55 -0600

    Proyecto final

---
+++ Always_Coffee_Time-master/Always_Coffee_Time/Cafeteria.BL/DatosdeInicio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeteria.BL
+{
+    public class DatosdeInicio: CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+            var nuevoUsuario = new Usuario();
+            nuevoUsuario.Nombre = "MariselaCruz";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("12345678");
+            
+
+            contexto.Usuarios.Add(nuevoUsuario);
+
+            base.Seed(contexto);
+        }
+    }
+}
